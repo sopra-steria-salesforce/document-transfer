1e921596c0dce9dc09684b38ada6a2e0307a9e90
petter-eikeland <97173615+petter-eikeland@users.noreply.github.com>
2025-06-17 13:55:39 +0200

    Revert previous change (#54)

--- force-app/lwc/fileUploadMulti/fileUploadMulti.js
+++ force-app/lwc/fileUploadMulti/fileUploadMulti.js
@@ -3,13 +3,16 @@ import {getFieldValue, getRecord, getRecordNotifyChange, notifyRecordUpdateAvail
 import CONTRACT_CATEGORY from "@salesforce/schema/Opportunity.Contract_Category__c";
 import CONTRACT_SERVICE_NOW_URL from '@salesforce/schema/Opportunity.ContractServiceNowUrl__c';
 import IS_CONTRACT_UPLOADED from '@salesforce/schema/Opportunity.IsContractUploaded__c';
+import NAVISION_SE from '@salesforce/schema/Opportunity.Navision_Customer_SE__c';
+import NAVISION_NO from '@salesforce/schema/Opportunity.Navision_Customer_NO__c';
+import NAVISION_DK from '@salesforce/schema/Opportunity.Navision_Customer_DK__c';
 import NAVISION_ACCOUNT from '@salesforce/schema/Opportunity.Navision_Account_in_Owners_Country__c';
 import OPPORTUNITY_OWNER_COUNTRY from '@salesforce/schema/Opportunity.Owner_Country__c';
 import STAGE_NAME from '@salesforce/schema/Opportunity.StageName';
 import {onError, subscribe, unsubscribe} from "lightning/empApi";
 import { RefreshEvent } from 'lightning/refresh';
 import {refreshApex} from "@salesforce/apex";
-const FIELDS = [IS_CONTRACT_UPLOADED,CONTRACT_SERVICE_NOW_URL,CONTRACT_CATEGORY,STAGE_NAME,'Opportunity.Account.Navision_Customer_SE__c','Opportunity.Account.Navision_Customer_NO__c','Opportunity.Account.Navision_Customer_DK__c',OPPORTUNITY_OWNER_COUNTRY,NAVISION_ACCOUNT];
+const FIELDS = [IS_CONTRACT_UPLOADED,CONTRACT_SERVICE_NOW_URL,CONTRACT_CATEGORY,STAGE_NAME,NAVISION_SE,NAVISION_DK,NAVISION_NO,OPPORTUNITY_OWNER_COUNTRY,NAVISION_ACCOUNT];
 
 export default class fileUploadMulti extends LightningElement {
     fields = FIELDS;
@@ -23,14 +26,11 @@ export default class fileUploadMulti extends LightningElement {
     channelName = '/event/Refresh_Custom_Components__e';
     @track navisionCheck = false;
     subscription = {}; // holds subscription, used for unsubscribe
-
-
     connectedCallback() {
-      //  this.showSpinner = true;
+        //  this.showSpinner = true;
         const self = this;
         const callbackFunction = function(response) {
             console.log('callbackFunction', response);
-
         }
         subscribe(this.channelName, -1, callbackFunction).then(response => {
             console.log('Subscribed to change events', response);
@@ -38,18 +38,15 @@ export default class fileUploadMulti extends LightningElement {
         self.refreshMyData();
         if(this.opportunity.data){
             this.showConditionalSection = this.opportunity.data.fields.StageName.value === '5 - Awarded' && (this.opportunity.data.fields.ContractServiceNowUrl__c.value === null || this.opportunity.data.fields.ContractServiceNowUrl__c.value === undefined || this.opportunity.data.fields.ContractServiceNowUrl__c.value === '');
-            this.navisionCheck = ((this.opportunity.data.fields.Account.Navision_Customer_DK__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Denmark') || (this.opportunity.data.fields.Account.Navision_Customer_SE__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Sweden') || (this.opportunity.data.fields.Account.Navision_Customer_NO__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Norway')) || this.opportunity.data.fields.Navision_Account_in_Owners_Country__c.value === true;
-            console.log(this.opportunity.data.fields.Account.Navision_Customer_NO__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Norway');
-            console.log(this.opportunity.data.fields.Account.Navision_Customer_SE__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Sweden');
-            console.log(this.opportunity.data.fields.Account.Navision_Customer_DK__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Denmark');
+            this.navisionCheck = ((this.opportunity.data.fields.Navision_Customer_DK__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Denmark') || (this.opportunity.data.fields.Navision_Customer_SE__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Sweden') || (this.opportunity.data.fields.Navision_Customer_NO__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Norway')) || this.opportunity.data.fields.Navision_Account_in_Owners_Country__c.value === true;
+            console.log(this.opportunity.data.fields.Navision_Customer_NO__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Norway');
+            console.log(this.opportunity.data.fields.Navision_Customer_SE__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Sweden');
+            console.log(this.opportunity.data.fields.Navision_Customer_DK__c.value === true && this.opportunity.data.fields.Owner_Country__c === 'Denmark');
             console.log(this.opportunity.data.fields.Navision_Account_in_Owners_Country__c.value === true);
         }
         refreshApex(this.opportunity).then(() => {
-
-
         });
     }
-
     refreshMyData() {
         refreshApex(this.opportunity).then(() => {
             if(this.opportunity.data){
@@ -59,8 +56,6 @@ export default class fileUploadMulti extends LightningElement {
             }
         });
     }
-
-
     @wire(getRecord, { recordId: '$recordId', fields: '$fields' })
     opportunity(result) {
         this.opportunity = result;
@@ -76,14 +71,13 @@ export default class fileUploadMulti extends LightningElement {
             console.log(this.opportunity.data.fields.Navision_Customer_DK__c.value === true && this.opportunity.data.fields.Owner_Country__c.value === 'Denmark');
         }
     }
-
-refresh(){
-    refreshApex(this.opportunity).then(() => {
-        console.log('refreshApex done');
-    });
-}
+    refresh(){
+        refreshApex(this.opportunity).then(() => {
+            console.log('refreshApex done');
+        });
+    }
     get showConditionalSection() {
-    console.log(this.opportunity.data);
+        console.log(this.opportunity.data);
         if (this.opportunity.data) {
             return this.opportunity.data.fields.StageName.value === '5 - Awarded' && (this.opportunity.data.fields.ContractServiceNowUrl__c.value === null || this.opportunity.data.fields.ContractServiceNowUrl__c.value === undefined || this.opportunity.data.fields.ContractServiceNowUrl__c.value === '');
         }
@@ -95,15 +89,12 @@ refresh(){
     get isContractUploaded() {
         return getFieldValue(this.opportunity.data, IS_CONTRACT_UPLOADED);
     }
-
     get stageName() {
         return getFieldValue(this.opportunity.data, STAGE_NAME);
     }
     get contractCategory() {
         return getFieldValue(this.opportunity.data, CONTRACT_CATEGORY);
     }
-
-
     handleClick(){
         window.open(this.opportunity.data.fields.ContractServiceNowUrl__c.value , '_blank');
     }
